692d67c94d31f5fae6a0795d68f9976dbd1c2657
Joel Fåk <joel.faak@gmail.com>
2025-12-13 21:31:06 +0100

    Day 10, moved parsing of input to separate function

--- 2025/joelfak-python/day10.py
+++ 2025/joelfak-python/day10.py
@@ -2,36 +2,50 @@
 
 import helpfunctions as hf
 import sys, pytest, logging, re
+from dataclasses import dataclass
 from itertools import combinations
 from collections import Counter
 
 logger = logging.getLogger(__name__)
 
-def find_smallest_number_of_presses(wanted_state, button_wiring):
-    logger.debug(f"wanted_state: {wanted_state}")
-    logger.debug(f"wiring: {len(button_wiring)}, {button_wiring}")
-    for num_presses in range(1, len(button_wiring) + 1):
-        for combination in combinations(button_wiring, num_presses):
-            tested_state = wanted_state + [button for buttons in combination for button in buttons]
+@dataclass
+class MachineConfiguration:
+    wanted_state: list
+    button_wiring: list
+    joltage: str
+
+def find_smallest_number_of_presses(mc: MachineConfiguration):
+    logger.debug(f"wanted_state: {mc.wanted_state}")
+    logger.debug(f"wiring: {len(mc.button_wiring)}, {mc.button_wiring}")
+    for num_presses in range(1, len(mc.button_wiring) + 1):
+        for combination in combinations(mc.button_wiring, num_presses):
+            tested_state = mc.wanted_state + [button for buttons in combination for button in buttons]
             if sum([v % 2 for v in Counter(tested_state).values()]) == 0:
                 logger.debug(f"Num presses: {num_presses}, combination: {combination}")
                 return num_presses
 
-@hf.timing
-def part1(data):
+def parse_input(data) -> MachineConfiguration:
     pattern = re.compile(r"\[(?P<wanted>[\.#]+)\] (?P<wiring>(?:\([\d,]+\) )+)\{(?P<joltage>[\d,]+)\}")
-    num_presses = 0
+    machine_configurations = []
+
     for line in data:
         result = pattern.search(line)
         logger.debug(f"\nline: {line}")
         if result:
-            wanted = [n for n, v in enumerate(result["wanted"]) if v == "#"]
-            wiring = [list(map(int, button_wiring.strip(")").strip("(").split(",")))
+            wanted_state = [n for n, v in enumerate(result["wanted"]) if v == "#"]
+            button_wiring = [list(map(int, button_wiring.strip(")").strip("(").split(",")))
                       for button_wiring in result["wiring"].split()]
-            logger.debug(f"wanted: {wanted}")
-            logger.debug(f"wiring: {wiring}")
-            num_presses += find_smallest_number_of_presses(wanted, wiring)
-    return num_presses
+            joltage = result["joltage"]
+            logger.debug(f"wanted_state: {wanted_state}")
+            logger.debug(f"button_wiring: {button_wiring}")
+            logger.debug(f"joltage: {joltage}")
+            machine_configurations.append(MachineConfiguration(wanted_state, button_wiring, joltage))
+    return machine_configurations
+
+@hf.timing
+def part1(data):
+    machine_configurations = parse_input(data)
+    return sum((find_smallest_number_of_presses(config) for config in machine_configurations))
 
 @hf.timing
 def part2(data):
@@ -45,12 +59,12 @@ def input():
             "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}",
             "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}]",]
 
-@pytest.mark.parametrize("test_input,expected", [(([1, 2],[[3],[1,3],[2],[2,3],[0,2],[0,1]]), 2),
-                                                 (([3],[(0,2,3,4),(2,3),(0,4),(0,1,2),(1,2,3,4)]), 3),
-                                                 (([1,2,3,5],[(0,1,2,3,4),(0,3,4),(0,1,2,4,5),(1,2)]), 2)
+@pytest.mark.parametrize("test_input,expected", [(MachineConfiguration([1, 2],[[3],[1,3],[2],[2,3],[0,2],[0,1]],[]), 2),
+                                                 (MachineConfiguration([3],[(0,2,3,4),(2,3),(0,4),(0,1,2),(1,2,3,4)],[]), 3),
+                                                 (MachineConfiguration([1,2,3,5],[(0,1,2,3,4),(0,3,4),(0,1,2,4,5),(1,2)],[]), 2)
                                                  ])
 def test_find_smallest_number_of_presses(test_input, expected):
-    assert find_smallest_number_of_presses(*test_input) == expected
+    assert find_smallest_number_of_presses(test_input) == expected
 
 def test_part1(input):
     assert part1(input) == 7
